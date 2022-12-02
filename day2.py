370545cc5adbfdacb7114628f15e18de0ef331d2
Merel Steenbergen <m.a.steenbergen@student.tudelft.nl>
2022-12-02 09:46:49 +0100

    Add shorter version of day-two

--- day2.py
+++ day2.py
@@ -49,4 +49,20 @@ def part_two():
                 result += 7
     return result
 
-print(part_two())
+def part_one_short():
+    inputs = [(ord(line[0]) - 64, ord(line[2]) - 87) for line in f]
+    sum_chosen = sum(b for (a,b) in inputs)
+    sum_win = sum([6 for (a,b) in inputs if (a == 1 and b == 2) or (a == 2 and b == 3) or (a == 3 and b == 1)])
+    sum_draw = sum([3 for (a,b) in inputs if a == b])
+    return sum_chosen + sum_draw + sum_win
+
+def part_two_short():
+    inputs = [(ord(line[0]) - 65, ord(line[2]) - 88) for line in f]
+    sum_win_draw_lose = sum(b*3 for (a,b) in inputs)
+    sum_rock = sum([1 for (a,b) in inputs if (a == 0 and b == 1) or (a == 1 and b == 0) or (a == 2 and b == 2)])
+    sum_paper = sum([2 for (a,b) in inputs if (a == 0 and b == 2) or (a == 1 and b == 1) or (a == 2 and b == 0)])
+    sum_scissors = sum([3 for (a,b) in inputs if (a == 0 and b == 0) or (a == 1 and b == 2) or (a == 2 and b == 1)])
+    return sum_win_draw_lose + sum_rock + sum_paper + sum_scissors
+
+
+print(part_two_short())
